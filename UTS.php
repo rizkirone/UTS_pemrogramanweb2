ecc06380a0f4b8f622ab494a195ca3e1d1b3604a
rizkirone <coyrizkicoy.com>
2022-04-21 22:19:55 +0700

    uts

---
+++ UTS.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Simpan Data</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td>Wilayah : </td>
+                <td><input type="text" name="wilayah"></td>
+                        
+            </tr>
+            <tr>
+                <td>Jumlah Positif : </td>
+                <td><input type="text" name="positif"></td>
+            </tr>
+            <tr>
+                <td>Jumlah Dirawat : </td>
+                <td><input type="text" name="rawat"></td>
+            </tr>
+            <tr>
+                <td>Jumlah Sembuh : </td>
+                <td><input type="text" name="sembuh"></td>
+            </tr>
+            <tr>
+                <td>Jumlah Meninggal : </td>
+                <td><input type="text" name="die"></td>
+            </tr>
+            <tr>
+                <td>Nama Operator : </td>
+                <td><input type="text" name="operator"></td>
+            </tr>
+            <tr>
+                <td>NIM Mahasiswa : </td>
+                <td><input type="text" name="nim"></td>
+            </tr>
+            <tr>
+                <td><button type="submit" name="OK">Simpan</button></td>
+            </tr>
+    </form>
+
+    <?php
+    if (isset($_POST['OK'])) {
+        if (empty($_POST['wilayah'])) {
+            print "Lengkapi form";
+        } else {
+            //Mendapatkan value dari data diatas
+            $wil = $_POST['wilayah'];
+            $pos = $_POST['positif'];
+            $rawat = $_POST['rawat'];
+            $sembuh = $_POST['sembuh'];
+            $die = $_POST['die'];
+            $op = $_POST['operator'];
+            $nim = $_POST['nim'];
+
+            //membuat waktu
+            $tanggal = date('d-m-Y  h:i:s a');
+
+            //membuat dan membuka file uts.txt
+            $buka = fopen("uts.txt", 'a');
+            echo "<script>window.location='uts.txt'</script>";
+
+            //menulis data yang telah dimasukkan
+            fwrite($buka, "Data Pemantaun Covid19 Wilayah ${wil} ");
+            fwrite($buka, "Per ${tanggal} ");
+            fwrite($buka, "${op} / ${nim} ");
+            fwrite($buka, "Positif   : ${pos} ");
+        fwrite($buka, "Dirawat   : ${rawat} ");
+        fwrite($buka, "Sembuh    :${sembuh}");
+        fwrite($buka, "Meninggal : ${die} ");
+        fwrite($buka, "");
+	
+
+            //menutup file 
+            fclose($buka);
+
+            //memunculkan kata
+            print "data berhasil disimpan";
+        }
+    } ?>
+</body>
+
+</html>
